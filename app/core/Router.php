9ed5dbc194da70ae03119f5239030c90a377e0fd
enriconathanael <kiakkiakchontoloi@gmail.com>
2026-01-29 13:02:40 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -2,21 +2,27 @@
 
 namespace App\Core;
 
+use App\Controllers\StudentController;
+
 class Router
 {
 
-    public function run()
+    public function run(): void
     {
         $method = $_SERVER['REQUEST_METHOD'];
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
         if($method == 'GET' && $uri == '/students') {
             require_once './app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller->index();
             return;
         }
 
         if($method == 'GET' && $uri == '/students/create') {
-           
+             require_once './app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller->create();   
             return;
         }
 
