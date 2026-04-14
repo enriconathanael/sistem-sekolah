19b9aa670e79fcee017070cc463137cb322b2be7
enriconathanael <kiakkiakchontoloi@gmail.com>
2026-04-14 13:16:19 +0700

    feat: implement student index page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,29 +1,36 @@
 <?php
     namespace App\Controllers;
     require_once '../app/core/Controller.php';
+    require_once '../app/models/Student.php';
 
     use App\core\Controller;
+    use App\models\Student;
 
     class StudentController extends Controller
     {
         public function index()
         {
-            $this->view('../students.index');
+            $studentModel = new Student();
+            $students = $studentModel->getStudents();
+
+            $this->view('students.index', [
+            'students'=> $students
+            ]);
         }
 
         public function create()
         {
-            $this->view('../students.create');
+            $this->view('students.create');
         }
 
         public function show(string $id)
         {
-            $this->view('../students.show');
+            $this->view('students.show');
         }
 
         public function edit(string $id)
         {
-            $this->view('../students.edit');
+            $this->view('students.edit');
         }
 
 
--- app/views/students/index.php
+++ app/views/students/index.php
@@ -1,40 +1,52 @@
-<div class="mt-8 space-y-2">
-            <!-- Card Header Start-->
-             <div class="p-4 shadow rounded-lg bg-white">
-                <h1 class="text-2xl font-bold ">Daftar Siswa</h1>
-                <p>Menampilkan daftar siswa yang terdaftar</p>
-            </div>
-             <!-- Card Header End -->
-            
-             <!-- Card Body Start -->
-              <div class="bg-white shadow rounded-lg">
+ <div class="mt-8 space-y-2">
+        <!-- Card Header Start -->
+        <div class="bg-white rounded-lg p-4 shadow">
+            <h1 class="text-2xl font-bold">Daftar Siswa</h1>
+            <p>Menampilkan daftar siswa yang terdaftar</p>
+        </div>
+        <!-- Card Header End  -->
+ 
+ 
+            <!-- Card Body Start  -->
+            <div class="bg-white shadow rounded-lg bg-white">
                 <table class="w-full">
                     <thead class="bg-gray-200">
                         <tr>
-                            <th class="px-4 py-2 text-left">No</th>
-                            <th class="px-4 py-2 text-left">Nama</th>
-                            <th class="px-4 py-2 text-left">NIS</th>
-                            <th class="px-4 py-2 text-left">Kelas</th>
-                            <th class="px-4 py-2 text-left">No Telepon</th>
-                            <th class="px-4 py-2">Aksi</th>
-                        </tr>
-                    </thead>
+                        <th class="px-4 py-2 text-left">No</th>
+                        <th class="px-4 py-2 text-left">Nama</th>
+                        <th class="px-4 py-2 text-left">NIS</th>
+                        <th class="px-4 py-2 text-left">Kelas</th>
+                        <th class="px-4 py-2 text-left">No Telepon</th>
+                        <th class="px-4 py-2 text-left">Aksi</th>
+                       </tr>
+                </thead>
                     <tbody>
-                        <td class="px-4 py-2 text">1</td>
-                        <td class="px-4 py-2 text">Andi</td>
-                        <td class="px-4 py-2 text">6767</td>
-                        <td class="px-4 py-2 text">XI TKJ 2</td>
-                        <td class="px-4 py-2 text">08676767676</td>
-                        <td class="px-4 py-2">
-                            <div class="flex justify-center items-center gap-4">
-                                <a href="/students/1" class="text-green-500 shadow rounded-lg p-2">Detail</a>
-                                <a href="/students/1/edit" class="text-yellow-500 shadow rounded-lg p-2">Edit</a>
-                                <a href="" class="text-red-500 shadow rounded-lg p-2">Hapus</a>
-                            </div>
-                        </td>
+                        <?php foreach($students as $index => $student): ?>
+                            <tr>
+                            <td class="px-4 py-2 text-left">
+                                <?= $index + 1 ?>
+                            </td>
+                            <td class="px-4 py-2 text-left">
+                                <?= $student['name']?>
+                            </td>
+                            <td class="px-4 py-2 text-left">
+                                <?= $student['nis'] ?>
+                            </td>
+                            <td class="px-4 py-2 text-left">
+                                <?= $student['class'] ?>
+                            </td>
+                            <td class="px-4 py-2 text-left">
+                                <?= $student['phone_number'] ?>
+                            </td>
+                            <td class="px-4 py-2 text-left">
+                                <div class="flex justify-center items-center gap-4">
+                                    <a href="/students/<?= $student['id'] ?>" class="text-green-500">Detail</a>
+                                    <a href="/students/<?= $student['id'] ?>/edit" class="text-yellow-500">Edit</a>
+                                    <a href="" class="text-red-500">Hapus</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <?php endforeach?>
                     </tbody>
                 </table>
-              </div>
-                
-              <!-- Card Body End -->
-        </div>
+            </div>
